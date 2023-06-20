6466b8ca2611ce6764db4200e21bc1415560d6ce
Altaf Abdul Gaffar <altafgaffar@gmail.com>
2023-06-20 11:36:11 +0300

    DeepLearning.AI Lessons - updated

---
+++ DeeplearningAI/App/TestBard.py
@@ -0,0 +1,11 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+print(is_prime(38256832769032098607))
