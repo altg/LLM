55dba65160ed4b5a96432b8ab97f90e8bddc5044
Altaf Abdul Gaffar <altaf@Rushdha-MacBook-Pro.local>
2023-08-01 18:58:29 +1000

    Llama models

---
+++ Llama/T2-Llama.py
@@ -0,0 +1,9 @@
+from llama_cpp import Llama
+
+llm = Llama(
+    model_path="/Users/altaf/Projects/LLM/Llama/llama-2-7b-ggml/llama-2-7b.ggmlv3.q4_0.bin"
+)
+
+response = llm("When was the moon landing?")
+
+print(response['choices'][0]["text"])
